8ce3cfe2637f8620de558e93015c8d4d8f7a6b57
Rodriguez <drodri18@ndc.nasa.gov>
2018-04-26 15:34:51 -0600

    17 cal ML model

---
+++ app.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pickle
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/17cal',methods=['POST','GET'])
+def make_predict():
+    if request.method=='POST':
+
+        new_vector = np.zeros(52)
+
+        velocity = request.form['velocity']
+        range1 = request.form['range']
+        sabot = request.form['sabot']
+        lpmass = request.form['Lpmass']
+        sabot = float(sabot)
+        lpmass = float(lpmass)
+        velocity = int(velocity)
+        range1 = int(range1)
+        
+        new_vector[velocity] = 1
+
+        new_vector[range1] = 1
+
+        #Min Max Scaler
+
+        sabot_OD = (sabot-0.174500)/(0.179500 - 0.174500)
+
+        Lp_mass = (lpmass-0.034300)/(0.109810 - 0.034300)
+
+
+        new_vector[50] = sabot_OD
+
+        new_vector[51] = Lp_mass
+
+        knn_pkl = open("KNNRegressionModel17Cal.pkl","rb")
+        model = pickle.load(knn_pkl)
+
+        predict = np.array([new_vector])
+        y_hat = model.predict(predict) 
+        output = y_hat[0]
+
+        return render_template('result.html', output=output)
+
+if __name__ == '__main__':
+    
+    app.run()          
